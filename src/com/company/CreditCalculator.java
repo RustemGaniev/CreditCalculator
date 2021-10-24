680778f3add5793ded422d4463324cad926a810a
RustemGaniev <ganievrr@gmail.com>
2021-10-24 15:07:59 +0300

    Version with Calculator

--- src/com/company/CreditCalculator.java
+++ src/com/company/CreditCalculator.java
@@ -3,14 +3,19 @@ package com.company;
 public class CreditCalculator {
 
     public int monthPay(int loan, int month, int monthBankInterest) {
-        return -1000;
+
+        return loan * (monthBankInterest + (
+                monthBankInterest / (1 + monthBankInterest) * month - 1 ));
     }
 
     public int totalPay(int loan, int month, int monthBankInterest) {
-        return -100000;
+        return month * loan * (monthBankInterest + (
+                monthBankInterest / (1 + monthBankInterest) * month - 1 ));
     }
 
     public int payOver(int loan, int month, int monthBankInterest) {
-        return -1000000;
+
+        return month * loan * (monthBankInterest + (
+                monthBankInterest / (1 + monthBankInterest) * month - 1 )) - loan;
     }
 }
